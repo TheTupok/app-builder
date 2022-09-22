55acad3d8d490792c14d8e4989c437f07027137b
Anatoly Tereshonok <ter.tolya@mail.ru>
2022-09-22 14:02:00 +0300

    Add style close button

--- src/app/pages/main-page.component/main-page.component.ts
+++ src/app/pages/main-page.component/main-page.component.ts
@@ -25,7 +25,6 @@ export class MainPageComponent implements OnInit {
       const target = event.target as HTMLElement;
       if (target.classList.contains('sample')) {
         const newTarget = this.workingFieldComponent.renderComponent(target)
-        console.log(newTarget)
         this.dragsService.DragAndDrop(event, newTarget)
       }
     }
