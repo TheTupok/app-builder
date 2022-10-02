bbf3855d8b61a2223f86ac264eb933c8862aafcc
Anatoly Tereshonok <ter.tolya@mail.ru>
2022-10-02 14:10:44 +0300

    Maximum resize by parent element

--- src/app/services/resize-component.service.ts
+++ src/app/services/resize-component.service.ts
@@ -12,18 +12,29 @@ export class ResizeComponentService {
   public resizeComponent(event: MouseEvent) {
 
     this.isResizing = true;
+
+    const minimumResizePx: number = 30
+
     const currentResize = event.target as HTMLElement
-    let ele: any = currentResize.parentNode.parentNode;
-    const minimumResizePx: number = 20
+    const ele = currentResize.parentElement.parentElement as HTMLElement;
+    const parentEle = ele.parentElement as HTMLElement;
 
-    const x = event.clientX;
-    const y = event.clientY;
+    const shiftX = event.offsetX - currentResize.getBoundingClientRect().width
+    const shiftY = event.offsetY - currentResize.getBoundingClientRect().height
 
-    const styles = window.getComputedStyle(ele);
-    const width = parseInt(styles.width, 10);
-    const height = parseInt(styles.height, 10);
-    const left = parseInt(styles.left, 10);
-    const top = parseInt(styles.top, 10);
+    const stylesEle = window.getComputedStyle(ele);
+    const width = parseInt(stylesEle.width);
+    const height = parseInt(stylesEle.height);
+    const left = parseInt(stylesEle.left);
+    const top = parseInt(stylesEle.top);
+
+    const stylesParent = window.getComputedStyle(parentEle)
+    const parentWidth = parseInt(stylesParent.width, 10);
+    const parentHeight = parseInt(stylesParent.height, 10);
+
+    const x = event.clientX - parentEle.getBoundingClientRect().left - shiftX;
+    const y = event.clientY - parentEle.getBoundingClientRect().top - shiftY;
+
 
     document.addEventListener('mousemove', mouseMoveHandler);
     document.addEventListener('mouseup', () => {
@@ -32,23 +43,36 @@ export class ResizeComponentService {
     });
 
     function mouseMoveHandler(e: MouseEvent) {
-      const dx = e.clientX - x;
-      const dy = e.clientY - y;
+      const dx = (e.clientX - parentEle.getBoundingClientRect().left) - x - shiftX;
+      const dy = (e.clientY - parentEle.getBoundingClientRect().top) - y - shiftY;
+
 
       if (currentResize.classList.contains('se')) {
         if ((width + dx) > minimumResizePx) ele.style.width = `${width + dx}px`;
         if ((height + dy) > minimumResizePx) ele.style.height = `${height + dy}px`;
+        if ((parentWidth - dx - x) < 0) ele.style.width = `${parentWidth + width - x}px`
+        if ((parentHeight - dy - y) < 0) ele.style.height = `${parentHeight + height - y}px`
       } else if (currentResize.classList.contains("sw")) {
         if ((height + dy) > minimumResizePx) ele.style.height = `${height + dy}px`;
         if ((width - dx) > minimumResizePx) {
           ele.style.width = `${width - dx}px`;
           ele.style.left = `${left + dx}px`
         }
+        if (left + dx <= 0) {
+          ele.style.width = `${width + x - currentResize.getBoundingClientRect().width}px`;
+          ele.style.left = `0px`
+        }
+        if ((parentHeight - dy - y) < 0) ele.style.height = `${parentHeight + height - y}px`
       } else if (currentResize.classList.contains("ne")) {
         if ((width + dx) > minimumResizePx) ele.style.width = `${width + dx}px`;
         if ((height - dy) > minimumResizePx) {
           ele.style.height = `${height - dy}px`;
           ele.style.top = `${top + dy}px`;
+        }
+        if ((parentWidth - dx - x) < 0) ele.style.width = `${parentWidth + width - x}px`
+        if (top + dy <= 0) {
+          ele.style.top = `0px`
+          ele.style.height = `${height + y - currentResize.getBoundingClientRect().height}px`;
         }
       } else {
         if ((width - dx) > minimumResizePx) {
@@ -58,6 +82,14 @@ export class ResizeComponentService {
         if ((height - dy) > minimumResizePx) {
           ele.style.height = `${height - dy}px`;
           ele.style.top = `${top + dy}px`;
+        }
+        if (left + dx <= 0) {
+          ele.style.left = `0px`
+          ele.style.width = `${width + x - currentResize.getBoundingClientRect().width}px`;
+        }
+        if (top + dy <= 0) {
+          ele.style.top = `0px`
+          ele.style.height = `${height + y - currentResize.getBoundingClientRect().height}px`;
         }
       }
     }
